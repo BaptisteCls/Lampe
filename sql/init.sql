5b42f680f0c67e5fd8a20b5f44c04e0f4314a33d
BaptisteCls <BaptisteCls>
2024-02-05 15:32:13 +0100

    Separation create.sql en create.sql et init.sql et reglage mauvais type creation de table

---
+++ sql/init.sql
@@ -0,0 +1,13 @@
+-- Supprimer la base website
+-- DROP DATABASE IF EXISTS website;
+
+-- La recreer
+-- CREATE DATABASE website;
+
+-- Se connecter dessus
+-- \c website;
+
+-- Puis executer le script suivant
+
+DROP TABLE IF EXISTS panier;
+DROP TABLE IF EXISTS items;
